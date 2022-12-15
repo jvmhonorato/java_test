70a855043191551cede81fe99783b77fc7e3e7c2
vituhonorato <vituhonorato.89@gmail.com>
2022-12-15 02:52:50 -0300

    testando o java

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+     Scanner teclado = new Scanner(System.in);
+        System.out.println("Qual produto deseja cadastrar?");
+        System.out.println("Qual quantidade do produto:");
+        System.out.println("Insira a quantidade do produto:");
+
+     teclado.close();
+} 
+}
